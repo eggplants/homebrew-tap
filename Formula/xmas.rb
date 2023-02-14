e5b793b7e788dfc7470afa27efd902e2363dec4c
eggplants <w10776e8w@yahoo.co.jp>
2023-02-15 00:52:50 +0900

    init

---
+++ Formula/xmas.rb
@@ -0,0 +1,21 @@
+class Xmas < Formula
+  desc "Plant X'mas tree(s) on your Terminal"
+  homepage "https://github.com/eggplants/sh-xmas"
+  url "https://github.com/eggplants/sh-xmas/archive/refs/tags/0.0.5.tar.gz"
+  sha256 "81f4b400c2dc710e10088dc2d79a1b7095a52ab0064726f30060e8aa94e9c2ae"
+  license "MIT"
+  head "https://github.com/eggplants/sh-xmas.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+(-\d+)?)$/i)
+  end
+
+  def install
+    bin.install "xmas"
+  end
+
+  test do
+    assert_match /^\d+\.\d+\.\d+$/, shell_output("#{bin}/xmas --version").strip
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# `eggplants/tap`
+
+```sh
+brew install eggplants/tap/<formula>
+brew install --cask eggplants/tap/<cask>
+```
+
+OR
+
+```sh
+brew tap eggplants/tap # at once
+
+brew install <formula>
+brew install --cask <cask>
+```
