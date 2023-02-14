bde7987fe86d16632ba6c0e5aeb8678af1e03118
eggplants <w10776e8w@yahoo.co.jp>
2023-02-15 00:52:50 +0900

    init

---
+++ Casks/mario-vs-luigi.rb
@@ -0,0 +1,19 @@
+cask "mario-vs-luigi" do
+  version "1.7.1.0-beta"
+  sha256 "fcd77cbe26216a0deffe1885ee6528b6fbabddb572dfbfb35e0281a3cdcd2a87"
+
+  url "https://github.com/ipodtouch0218/NSMB-MarioVsLuigi/releases/download/v#{version}/MarioVsLuigi-MacOS-v#{version}.zip",
+      verified: "github.com/ipodtouch0218/NSMB-MarioVsLuigi"
+  name "New Super Mario Bros. - Mario Vs Luigi"
+  desc "Standalone Unity remake of New Super Mario Bros DS' multiplayer gamemode"
+  homepage "https://ipodtouch0218.itch.io/nsmb-mariovsluigi"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "MarioVsLuigi-MacOS.app"
+
+  zap trash: "~/Library/Application Support/MarioVsLuigi-MacOS*"
+end
---
+++ README.md
@@ -0,0 +1,25 @@
+# `eggplants/tap`
+
+[![test](
+  <https://github.com/eggplants/homebrew-tap/actions/workflows/test.yml/badge.svg>
+  )](
+  <https://github.com/eggplants/homebrew-tap/actions/workflows/test.yml>
+) [![update formula](
+  <https://github.com/eggplants/homebrew-tap/actions/workflows/update_formula.yml/badge.svg>
+  )](
+  <https://github.com/eggplants/homebrew-tap/actions/workflows/update_formula.yml>
+)
+
+```sh
+brew install eggplants/tap/<formula>
+brew install --cask eggplants/tap/<cask>
+```
+
+OR
+
+```sh
+brew tap eggplants/tap # at once
+
+brew install <formula>
+brew install --cask <cask>
+```
