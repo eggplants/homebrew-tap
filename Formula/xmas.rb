d4a39e9432e5c4bd5376833101beb34686128d4d
eggplants <w10776e8w@yahoo.co.jp>
2023-02-15 02:15:39 +0900

    fix: follow brew style

--- Formula/xmas.rb
+++ Formula/xmas.rb
@@ -2,9 +2,9 @@ class Xmas < Formula
   desc "Plant X'mas tree(s) on your Terminal"
   homepage "https://github.com/eggplants/sh-xmas"
   url "https://github.com/eggplants/sh-xmas/archive/refs/tags/0.0.5.tar.gz"
+  version "0.0.5"
   sha256 "81f4b400c2dc710e10088dc2d79a1b7095a52ab0064726f30060e8aa94e9c2ae"
   license "MIT"
-  version "0.0.5"
   head "https://github.com/eggplants/sh-xmas.git", branch: "master"
 
   livecheck do
