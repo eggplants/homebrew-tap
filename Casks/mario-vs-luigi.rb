11251fa6076080e30a8ae85aebf8b3fd7e499de9
eggplants <w10776e8w@yahoo.co.jp>
2023-09-06 07:37:32 +0900

    Update mario-vs-luigi.rb

--- Casks/mario-vs-luigi.rb
+++ Casks/mario-vs-luigi.rb
@@ -3,7 +3,7 @@ cask "mario-vs-luigi" do
   sha256 "fcd77cbe26216a0deffe1885ee6528b6fbabddb572dfbfb35e0281a3cdcd2a87"
 
   url "https://github.com/ipodtouch0218/NSMB-MarioVsLuigi/releases/download/v#{version}/MarioVsLuigi-MacOS-v#{version}.zip",
-      verified: "github.com/ipodtouch0218/NSMB-MarioVsLuigi"
+      verified: "github.com/ipodtouch0218/NSMB-MarioVsLuigi/"
   name "New Super Mario Bros. - Mario Vs Luigi"
   desc "Standalone Unity remake of New Super Mario Bros DS' multiplayer gamemode"
   homepage "https://ipodtouch0218.itch.io/nsmb-mariovsluigi"
